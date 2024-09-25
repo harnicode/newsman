b53b7010a6d012be4ee489b19d198347d712840e
shawlycode <kutinaorlando@gmail.com>
2024-09-25 22:51:58 +0000

    initial commit

---
+++ lib/home/home_bloc.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+//Define the Homestate
+class HomeState {
+  final Set<String> selections;
+  HomeState({required this.selections});
+
+  @override
+  String toString() {
+    return "HomeState($selections)";
+  }
+
+  HomeState copyWith(Set<String>? selections) {
+    return HomeState(
+      selections: selections ?? this.selections,
+    );
+  }
+}
+
+//Define event for Home
+abstract class HomeEvent {}
+
+class HandleTap extends HomeEvent {
+  final String id;
+  HandleTap(this.id);
+}
+
+//Define the Bloc to manage the home state
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeState(selections: {})) {
+    on<HandleTap>(_onToggleSelection);
+  }
+//Now handle the HandleTap to update the state
+  void _onToggleSelection(HandleTap event, Emitter<HomeState> emit) {
+    final newSelections = Set<String>.from(state.selections);
+
+    //define condition to track event
+    if (newSelections.contains(event.id)) {
+      newSelections.remove(event.id);
+    } else {
+      newSelections.add(event.id);
+    }
+    emit(state.copyWith(newSelections));
+  }
+}
